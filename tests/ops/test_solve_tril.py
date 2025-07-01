21b33496908d90a9a661b2d11e67b5f7ae8a95ad
Yu Zhang <yzhang.cs@outlook.com>
2025-07-01 15:58:27 +0800

    [GDN] Fuse 64x64 matrix inverse kernel (#501)

--- tests/ops/test_solve_tril.py
+++ tests/ops/test_solve_tril.py
@@ -26,10 +26,6 @@ from fla.utils import assert_close, device, device_platform
     ]
 )
 @pytest.mark.skipif(
-    os.getenv('SKIP_TEST_CHUNK_VARLEN') == '0',
-    reason='Skipping test because TEST_CHUNK_VARLEN is enabled'
-)
-@pytest.mark.skipif(
     device_platform == 'intel',
     reason='Intel Pytorch Failure'
 )
@@ -41,11 +37,13 @@ def test_solve_tril(B, T, H, chunk_size):
     k_padded = F.pad(k, (0, 0, 0, padding_size, 0, 0, 0, 0))
     k_padded = k_padded.reshape(B, H, -1, chunk_size, 64)
     A = (k_padded @ k_padded.transpose(-1, -2)).tril(-1)
-    Ai = solve_tril(A.reshape(B, H, -1, chunk_size)[:, :, :T, :].transpose(1, 2)).transpose(1, 2)
 
-    Ai_ref = torch.inverse(A + torch.eye(A.shape[-1], device=A.device)[None, None, None, ...])
-    Ai_ref = Ai_ref.reshape(B, H, -1, chunk_size)[:, :, :T, :]
-    assert_close('solve_tril', Ai, Ai_ref, 0.0001)
+    ref = torch.inverse(A + torch.eye(A.shape[-1], device=A.device)[None, None, None, ...])
+    ref = ref.reshape(B, H, -1, chunk_size)[:, :, :T, :]
+
+    tri = solve_tril(A.reshape(B, H, -1, chunk_size)[:, :, :T, :].transpose(1, 2)).transpose(1, 2)
+
+    assert_close('solve_tril', ref, tri, 0.0001)
 
 
 @pytest.mark.parametrize(
@@ -81,14 +79,15 @@ def test_solve_tril_varlen(
     k = F.normalize(torch.randn((1, T, H, D), dtype=torch.bfloat16, device=device), dim=-1)
     beta = torch.randn((1, T, H), dtype=torch.bfloat16, device=device).sigmoid()
     A = chunk_scaled_dot_kkt_fwd(k, beta, cu_seqlens=cu_seqlens, chunk_size=chunk_size)
-    Ai = solve_tril(A, cu_seqlens=cu_seqlens)
 
-    Ai_ref = torch.zeros_like(Ai)
+    ref = torch.zeros_like(A)
     for i in range(len(cu_seqlens) - 1):
         for j in range(cu_seqlens[i], cu_seqlens[i+1], chunk_size):
             actual_size = min(chunk_size, cu_seqlens[i+1] - j)
-            Ai_ref[:, j:j+actual_size, :, :actual_size] = torch.inverse(
+            ref[:, j:j+actual_size, :, :actual_size] = torch.inverse(
                 A[:, j:j+actual_size, :, :actual_size].transpose(1, 2) +
                 torch.eye(actual_size, device=A.device, dtype=A.dtype)[None, None, ...]
             ).transpose(1, 2)
-    assert_close('solve_tril_varlen', Ai, Ai_ref, 0.0001)
+
+    tri = solve_tril(A, cu_seqlens=cu_seqlens)
+    assert_close('solve_tril_varlen', ref, tri, 0.0001)
