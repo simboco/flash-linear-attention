[PaTH] Improving precision to fix NaNs

fla/layers/path_attn.py
@@ -65,10 +65,10 @@ class PaTHAttention(nn.Module):
         else:
             self.w_proj = nn.Linear(self.hidden_size, self.kv_dim, bias=False)
 
-        # TODO: per head norm?
+        # per head norm
         if use_qk_norm:
-            self.maybe_q_norm = RMSNorm(self.hidden_size)
-            self.maybe_k_norm = RMSNorm(self.kv_dim)
+            self.maybe_q_norm = RMSNorm(self.head_dim)
+            self.maybe_k_norm = RMSNorm(self.head_dim)
         else:
             self.maybe_q_norm = nn.Identity()
             self.maybe_k_norm = nn.Identity()
@@ -104,9 +104,8 @@ class PaTHAttention(nn.Module):
         k = self.k_proj(hidden_states)
         v = self.v_proj(hidden_states)
         w = self.w_proj(hidden_states)
-        beta = self.bt_proj(hidden_states).sigmoid() * 2  # allowing negative eigenvalues
+        beta = self.bt_proj(hidden_states).float().sigmoid() * 2  # allowing negative eigenvalues
         g = F.logsigmoid(self.g_proj(hidden_states).float()) if self.use_forget_gate else None
-        q, k = self.maybe_q_norm(q), self.maybe_k_norm(k)
         cu_seqlens = kwargs.get('cu_seqlens', None)
         assert not (cu_seqlens is not None and attention_mask is not None), (
             "cu_seqlens should not be provided when attention_mask is not None"
@@ -120,7 +119,8 @@ class PaTHAttention(nn.Module):
             k = rearrange(k, '... (h d) -> ... h d', d=self.head_dim)
             v = rearrange(v, '... (h d) -> ... h d', d=self.head_dim)
             w = rearrange(w, '... (h d) -> ... h d', d=self.head_dim)
-            w = l2_norm(w)
+            q, k = self.maybe_q_norm(q), self.maybe_k_norm(k)
+            w = l2_norm(w, output_dtype=torch.float32)
             o, _ = parallel_path_attn(q=q, k=k, v=v, w=w, beta=beta, g=g, cu_seqlens=cu_seqlens)
 
         # Prefilling or decoding
@@ -136,12 +136,13 @@ class PaTHAttention(nn.Module):
                     past_k, past_v, past_g = last_state['attn_state']
                 else:
                     past_k, past_v = last_state['attn_state']
+                    past_g = None
                 w_conv_state = last_state['conv_state']
                 past_k = rearrange(past_k, '... (h d) -> ... h d', d=self.head_dim)
                 if self.use_w_shortconv:
                     w, w_conv_state = self.w_conv1d(w, cache=w_conv_state, output_final_state=use_cache, cu_seqlens=cu_seqlens)
                 w = rearrange(w, '... (h d) -> ... h d', d=self.head_dim)
-                w = l2_norm(w)
+                w = l2_norm(w, output_dtype=torch.float32)
 
                 @torch.compile
                 def rank_one_update(k, w, beta):
@@ -198,7 +199,7 @@ class PaTHAttention(nn.Module):
                 k = rearrange(k, '... (h d) -> ... h d', d=self.head_dim)
                 v = rearrange(v, '... (h d) -> ... h d', d=self.head_dim)
                 w = rearrange(w, '... (h d) -> ... h d', d=self.head_dim)
-                w = l2_norm(w)
+                w = l2_norm(w, output_dtype=torch.float32)
                 o, k_cache = parallel_path_attn(q=q, k=k, v=v, w=w, beta=beta, g=g,
                                                 cu_seqlens=cu_seqlens, use_cache=use_cache)
                 if use_cache:

fla/ops/path_attn/cumprod_householder_fwd.py
@@ -105,7 +105,7 @@ def chunk_cumprod_householder_fwd_fn(
     grid = (NS, H)
     hc_whole = torch.empty((NS, H, K, K), device=k.device, dtype=w1.dtype)
     k_new = torch.empty_like(k, dtype=k.dtype)
-    hc_suffix = torch.empty((NT, H, K, K), device=k.device, dtype=w2.dtype)
+    hc_suffix = torch.empty((NT, H, K, K), device=k.device, dtype=w1.dtype)
     chunk_cumprod_householder_fwd_kernel[grid](
         k=k, k_new=k_new, w1=w1, w2=w2, hc_whole=hc_whole, hc_suffix=hc_suffix,
         cu_seqlens=cu_seqlens,

fla/ops/path_attn/intra_chunk_preprocess_bwd.py
@@ -12,7 +12,7 @@ from fla.utils import check_shared_mem
 })
 @triton.jit(do_not_specialize=['T'])
 def intra_chunk_preprocess_bwd_kernel(
-    q, k, w, beta,
+    q, k, w, w2, beta,
     AT,
     dA_local, dq, dq_new, dk, dk_new, dw, dbeta, dw1, dw2, T,
     offsets, indices,
@@ -39,9 +39,11 @@ def intra_chunk_preprocess_bwd_kernel(
     p_q = tl.make_block_ptr(q + (bos * HQ + i_hq) * K, (T, K), (K*HQ, 1), (i_t * BT, 0), (BT, BK), (1, 0))
     p_k = tl.make_block_ptr(k + (bos * H + i_h) * K, (T, K), (K*H, 1), (i_t * BT, 0), (BT, BK), (1, 0))
     p_w = tl.make_block_ptr(w + (bos * H + i_h) * K, (T, K), (K*H, 1), (i_t * BT, 0), (BT, BK), (1, 0))
+    p_w2 = tl.make_block_ptr(w2 + (bos * H + i_h) * K, (T, K), (K*H, 1), (i_t * BT, 0), (BT, BK), (1, 0))
     p_beta = tl.make_block_ptr(beta + (bos * H + i_h), (T, ), (H, ), (i_t * BT, ), (BT, ), (0, ))
     p_T = tl.make_block_ptr(AT + (bos * H + i_h) * BT, (T, BT), (BT*H, 1), (i_t * BT, 0), (BT, BT), (1, 0))
     b_w = tl.load(p_w, boundary_check=(0, 1))
+    b_Twb = tl.load(p_w2, boundary_check=(0, 1))
     b_beta = tl.load(p_beta, boundary_check=(0, ))
     b_q = tl.load(p_q, boundary_check=(0, 1))
     b_k = tl.load(p_k, boundary_check=(0, 1))
@@ -51,7 +53,6 @@ def intra_chunk_preprocess_bwd_kernel(
     o_i = tl.arange(0, BT)
     b_qw = tl.where(o_i[:, None] >= o_i[None, :], tl.dot(b_q, tl.trans(b_w)), 0).to(b_q.dtype)
     b_wbk = tl.where(o_i[:, None] > o_i[None, :], tl.dot(b_w_beta, tl.trans(b_k)), 0).to(b_k.dtype)
-    b_Twb = tl.dot(b_T, b_w_beta).to(b_w.dtype)
     b_Twbk = tl.dot(b_T, b_wbk).to(b_w.dtype)
 
     p_dA_local = tl.make_block_ptr(dA_local + (bos * HQ + i_hq) * BT, (T, BT), (BT*HQ, 1), (i_t * BT, 0), (BT, BT), (1, 0))
@@ -110,7 +111,7 @@ def intra_chunk_preprocess_bwd_kernel(
     tl.store(p_dbeta, b_dbeta.to(dbeta.dtype.element_ty), boundary_check=(0, ))
 
 
-def intra_chunk_preprocess_bwd_fn(q, k, w, beta,
+def intra_chunk_preprocess_bwd_fn(q, k, w, w2, beta,
                                   dq, dk, dA_local,
                                   dw1, dw2,
                                   A, L, D, do, scale, cu_seqlens=None):
@@ -129,7 +130,7 @@ def intra_chunk_preprocess_bwd_fn(q, k, w, beta,
     dq_new = torch.empty_like(dq, dtype=q.dtype)
 
     intra_chunk_preprocess_bwd_kernel[grid](
-        q=q, k=k, w=w, beta=beta,
+        q=q, k=k, w=w, w2=w2, beta=beta,
         AT=A,
         dA_local=dA_local, dq=dq, dq_new=dq_new, dk=dk, dk_new=dk_new, dw=dw, dbeta=dbeta, dw1=dw1, dw2=dw2, T=T,
         offsets=cu_seqlens, indices=indices,

fla/ops/path_attn/intra_chunk_preprocess_bwd_prepare.py
@@ -84,41 +84,33 @@ def chunk_transform_qk_bwd_kernel_prepare(
     p_q = tl.make_block_ptr(q, (T, K), (HQ*K, 1), (i_t * BT, 0), (BT, BK), (1, 0))
     p_k = tl.make_block_ptr(k, (K, T), (1, H*K), (0, i_t * BT), (BK, BT), (0, 1))
     p_w = tl.make_block_ptr(w, (T, K), (H*K, 1), (i_t * BT, 0), (BT, BK), (1, 0))
+    p_beta = tl.make_block_ptr(beta, (T, ), (H, ), (i_t * BT, ), (BT, ), (0, ))
+
     b_q = tl.load(p_q, boundary_check=(0, 1))
     b_kt = tl.load(p_k, boundary_check=(0, 1))
     b_w = tl.load(p_w, boundary_check=(0, 1))
+    b_beta = tl.load(p_beta, boundary_check=(0, ))
     p_T = tl.make_block_ptr(AT, (T, BT), (BT*H, 1), (i_t * BT, 0), (BT, BT), (1, 0))
-    b_T = tl.load(p_T, boundary_check=(0, 1))
+    b_T = tl.load(p_T, boundary_check=(0, 1)) * b_beta[None, :]
 
     o_i = tl.arange(0, BT)
     m_t = o_i[:, None] >= o_i[None, :]
-    p_beta = tl.make_block_ptr(beta, (T, ), (H, ), (i_t * BT, ), (BT, ), (0, ))
-    b_beta = tl.load(p_beta, boundary_check=(0, ))
-    b_w_beta = (b_w * b_beta[:, None]).to(b_w.dtype)
-
-    b_Twb = tl.dot(b_T.to(b_w_beta.dtype), b_w_beta).to(b_w_beta.dtype)
-
-    b_qw = tl.where(m_t, tl.dot(b_q, tl.trans(b_w)), 0).to(b_q.dtype)
-    b_qwT = tl.dot(b_qw, b_T).to(b_q.dtype)
-    b_wbk = tl.where(o_i[:, None] > o_i[None, :], tl.dot(b_w_beta, b_kt), 0).to(b_w.dtype)
+    b_qw = tl.where(m_t, tl.dot(b_q, tl.trans(b_w.to(b_q.dtype))), 0).to(b_q.dtype)
+    b_qwT = tl.dot(b_qw, b_T.to(b_q.dtype)).to(b_q.dtype)
+    b_wbk = tl.where(o_i[:, None] > o_i[None, :], tl.dot(b_w.to(b_kt.dtype), b_kt), 0).to(b_q.dtype)
     b_A = tl.where(m_t, tl.dot(b_q, b_kt) - tl.dot(b_qwT, b_wbk), 0)
 
-    b_q = b_q - tl.dot(b_qwT, b_w_beta)
+    b_q = b_q.to(tl.float32) - tl.dot(b_qwT, b_w.to(b_qwT.dtype))
     p_q_new = tl.make_block_ptr(q_new, (T, K), (K*HQ, 1), (i_t * BT, 0), (BT, K), (1, 0))
     tl.store(p_q_new, b_q.to(p_q_new.dtype.element_ty), boundary_check=(0, 1))
 
     if i_hq % G == 0:
-        if RETURN_H:
-            b_h = tl.dot(tl.trans(b_w), b_Twb)
-            p_h = tl.make_block_ptr(h, (K, K), (K, 1), (0, 0), (BK, BK), (1, 0))
-            tl.store(p_h, b_h.to(p_h.dtype.element_ty), boundary_check=(0, 1))
-        else:
-            p_h = tl.make_block_ptr(h, (T, K), (K * H, 1), (i_t * BT, 0), (BT, BK), (1, 0))
-            tl.store(p_h, b_Twb.to(p_h.dtype.element_ty), boundary_check=(0, 1))
-
-        b_T_wbk = tl.dot(b_T, b_wbk).to(b_w.dtype)
+        b_Twb = tl.dot(b_T, b_w) # tf32
+        p_h = tl.make_block_ptr(h, (T, K), (K * H, 1), (i_t * BT, 0), (BT, BK), (1, 0))
+        tl.store(p_h, b_Twb.to(p_h.dtype.element_ty), boundary_check=(0, 1))
+        b_T_wbk = tl.dot(b_T.to(b_wbk.dtype), b_wbk).to(b_kt.dtype)
         p_k_new = tl.make_block_ptr(k_new, (K, T), (1, K*H), (0, i_t * BT), (BK, BT), (0, 1))
-        tl.store(p_k_new, (b_kt - tl.dot(tl.trans(b_w), b_T_wbk)).to(p_k_new.dtype.element_ty), boundary_check=(0, 1))
+        tl.store(p_k_new, (b_kt - tl.dot(tl.trans(b_w.to(b_kt.dtype)), b_T_wbk)).to(p_k_new.dtype.element_ty), boundary_check=(0, 1))
 
     if USE_GATE:
         p_g_cumsum = tl.make_block_ptr(g_cumsum, (T, ), (HQ, ), (i_t * BT, ), (BT, ), (0, ))
@@ -164,12 +156,8 @@ def intra_chunk_preprocess_bwd_prepare_fn(q, k, v, w, beta, g_cumsum, A, L, D, d
     chunk_offsets = prepare_chunk_offsets(cu_seqlens, BT) if cu_seqlens is not None else None
     NT = triton.cdiv(T, BT) if cu_seqlens is None else len(indices)
     grid = (NT, B*HQ)
-    # better precision because h would be of norm smaller than 1 anyways
-    if return_h:
-        h = torch.empty(B, NT, H, K, K, dtype=w.dtype, device=q.device)
-    else:
-        h = torch.empty_like(w)
-    dA_local = torch.empty(B, T, HQ, BT, dtype=w.dtype, device=q.device)
+    h = torch.empty_like(w)
+    dA_local = torch.empty(B, T, HQ, BT, dtype=q.dtype, device=q.device)
     dv = torch.empty(B, T, HQ, V, device=q.device, dtype=torch.float32)
     dg_cumsum = torch.empty_like(g_cumsum) if g_cumsum is not None else None
 

fla/ops/path_attn/intra_chunk_preprocess_fwd.py
@@ -72,35 +72,36 @@ def intra_chunk_preprocess_fwd_kernel(
     p_k = tl.make_block_ptr(k, (K, T), (1, H*K), (0, i_t * BT), (BK, BT), (0, 1))
     p_w = tl.make_block_ptr(w, (T, K), (H*K, 1), (i_t * BT, 0), (BT, BK), (1, 0))
     p_v = tl.make_block_ptr(v, (T, V), (H*V, 1), (i_t * BT, 0), (BT, BV), (1, 0))
+    p_beta = tl.make_block_ptr(beta, (T, ), (H, ), (i_t * BT, ), (BT, ), (0, ))
+    p_T = tl.make_block_ptr(A, (T, BT), (BT*H, 1), (i_t * BT, 0), (BT, BT), (1, 0))
+
+    b_beta = tl.load(p_beta, boundary_check=(0, ))
     b_q = tl.load(p_q, boundary_check=(0, 1))
     b_kt = tl.load(p_k, boundary_check=(0, 1))
     b_v = tl.load(p_v, boundary_check=(0, 1))
     b_w = tl.load(p_w, boundary_check=(0, 1))
-    p_T = tl.make_block_ptr(A, (T, BT), (BT*H, 1), (i_t * BT, 0), (BT, BT), (1, 0))
     b_T = tl.load(p_T, boundary_check=(0, 1))
+    b_T = b_T * b_beta[None, :]
 
     o_i = tl.arange(0, BT)
     m_t = o_i[:, None] >= o_i[None, :]
-    p_beta = tl.make_block_ptr(beta, (T, ), (H, ), (i_t * BT, ), (BT, ), (0, ))
-    b_beta = tl.load(p_beta, boundary_check=(0, ))
-    b_w_beta = (b_w * b_beta[:, None])
 
-    b_qw = tl.where(m_t, tl.dot(b_q, tl.trans(b_w)), 0).to(b_q.dtype)
-    b_qwT = tl.dot(b_qw, b_T).to(b_q.dtype)
-    b_wbk = tl.where(o_i[:, None] > o_i[None, :], tl.dot(b_w_beta, b_kt), 0).to(b_q.dtype)
-    b_A = tl.where(m_t, tl.dot(b_q, b_kt) - tl.dot(b_qwT, b_wbk), 0)
+    b_qw = tl.where(m_t, tl.dot(b_q, tl.trans(b_w.to(b_q.dtype))), 0).to(b_q.dtype)
+    b_qwT = tl.dot(b_qw, b_T.to(b_q.dtype)).to(b_q.dtype)
+    b_wbk = tl.where(o_i[:, None] > o_i[None, :], tl.dot(b_w.to(b_q.dtype), b_kt), 0).to(b_q.dtype)
+    b_A = tl.where(m_t, tl.dot(b_q, b_kt) - tl.dot(b_qwT.to(b_q.dtype), b_wbk), 0)
 
-    b_q = b_q - tl.dot(b_qwT, b_w_beta)
+    b_q = b_q.to(tl.float32) - tl.dot(b_qwT, b_w.to(b_q.dtype))
     p_q_new = tl.make_block_ptr(q_new, (T, K), (K*HQ, 1), (i_t * BT, 0), (BT, K), (1, 0))
     tl.store(p_q_new, b_q.to(p_q_new.dtype.element_ty), boundary_check=(0, 1))
 
     if i_hq % G == 0:
-        b_Twb = tl.dot(b_T, b_w_beta).to(b_w.dtype)
+        b_Twb = tl.dot(b_T, b_w)
         p_w2 = tl.make_block_ptr(w2, (T, K), (K*H, 1), (i_t * BT, 0), (BT, BK), (1, 0))
-        tl.store(p_w2, b_Twb, boundary_check=(0, 1))
-        b_T_wbk = tl.dot(b_T, b_wbk).to(b_w.dtype)
+        tl.store(p_w2, b_Twb.to(p_w2.dtype.element_ty), boundary_check=(0, 1))
+        b_T_wbk = tl.dot(b_T.to(b_kt.dtype), b_wbk).to(b_kt.dtype)
         p_k_new = tl.make_block_ptr(k_new, (K, T), (1, K*H), (0, i_t * BT), (BK, BT), (0, 1))
-        tl.store(p_k_new, (b_kt - tl.dot(tl.trans(b_w), b_T_wbk)).to(p_k_new.dtype.element_ty), boundary_check=(0, 1))
+        tl.store(p_k_new, (b_kt - tl.dot(tl.trans(b_w.to(b_kt.dtype)), b_T_wbk)).to(p_k_new.dtype.element_ty), boundary_check=(0, 1))
 
     if USE_G:
         p_g_cumsum = tl.make_block_ptr(g_cumsum, (T, ), (HQ, ), (i_t * BT, ), (BT, ), (0, ))
@@ -121,13 +122,14 @@ def intra_chunk_preprocess_fwd_kernel(
     tl.store(p_l, l_i.to(p_l.dtype.element_ty), boundary_check=(0,))
 
 
+
 def intra_chunk_preprocess_fwd_fn(q, k, v, w, beta, g_cumsum, A, scale, BT, cu_seqlens):
     HQ = q.shape[-2]
     B, T, H, K = k.shape
     V = v.shape[-1]
-    q_new = torch.empty_like(q)
+    q_new = torch.empty_like(q, dtype=torch.float32) # for stability
     k_new = torch.empty_like(k)
-    o = torch.empty(B, T, HQ, V, device=q.device)
+    o = torch.empty(B, T, HQ, V, device=q.device, dtype=torch.float32)
 
     indices = prepare_chunk_indices(cu_seqlens, BT) if cu_seqlens is not None else None
     NT = triton.cdiv(T, BT) if cu_seqlens is None else len(indices)

fla/ops/path_attn/parallel.py
@@ -29,9 +29,11 @@ class ParallelPATHAttentionFunction(torch.autograd.Function):
     @input_guard
     @autocast_custom_fwd
     def forward(ctx, q, k, v, w, beta, g, scale, cu_seqlens, use_cache=False):
+
         g_cumsum = chunk_global_cumsum(g, cu_seqlens=cu_seqlens, output_dtype=torch.float32) if g is not None else None
         BS = 64 if check_shared_mem('hopper') else 32
         BT = 128 if check_shared_mem('ampere') else 64
+
         A = chunk_scaled_dot_kkt_fwd(
             k=w,
             beta=beta,
@@ -42,7 +44,7 @@ class ParallelPATHAttentionFunction(torch.autograd.Function):
         A = solve_tril(
             A=A,
             cu_seqlens=cu_seqlens,
-            output_dtype=w.dtype
+            output_dtype=w.dtype  # force fp32?
         )
         q_new, k_new, w2, o, L, M = intra_chunk_preprocess_fwd_fn(
             q=q,
@@ -56,13 +58,15 @@ class ParallelPATHAttentionFunction(torch.autograd.Function):
             BT=BS,
             cu_seqlens=cu_seqlens,
         )
+        w_fp16 = w.to(torch.float16)
+        w2_fp16 = w2.to(torch.float16)
         o, L = parallel_path_fwd_fn(
             q=q_new,
             k=k_new,
             v=v,
             L=L,
-            w1=w,
-            w2=w2,
+            w1=w_fp16,
+            w2=w2_fp16,
             M=M,
             o=o,
             g_cumsum=g_cumsum,
@@ -103,18 +107,20 @@ class ParallelPATHAttentionFunction(torch.autograd.Function):
             cu_seqlens=cu_seqlens,
             return_h=False,
         )
-
+        w_fp16 = w.to(torch.float16)
+        h_fp16 = h.to(torch.float16)
         k_new_large, hc_suffix, hc_whole = chunk_cumprod_householder_fwd_fn(
             k=k_new,
-            w1=w,
-            w2=h,
+            w1=w_fp16,
+            w2=h_fp16,
             S=S,
             BT=BS,
             cu_seqlens=cu_seqlens
         )
-
-        q_new_large = transform_q_fwd_fn(q=q_new, w1=w, w2=h, cu_seqlens=cu_seqlens, BT=BT, BS=BS, S=S)
-
+        q_new_large = transform_q_fwd_fn(q=q_new, w1=w_fp16, w2=h_fp16, cu_seqlens=cu_seqlens, BT=BT, BS=BS, S=S)
+        w = w.to(q.dtype)
+        h = h.to(q.dtype)
+        A = A.to(q.dtype)
         dk, dv, _ = parallel_path_bwd_dkv_fn(
             q=q_new_large,
             k=k_new_large,
@@ -184,6 +190,7 @@ class ParallelPATHAttentionFunction(torch.autograd.Function):
             q=q,
             k=k,
             w=w,
+            w2=h,
             beta=beta,
             dq=dq,
             dk=dk,
@@ -255,6 +262,9 @@ def parallel_path_attn(
     """
     if scale is None:
         scale = k.shape[-1]**-0.5
+    assert w.dtype == beta.dtype == torch.float32, 'w, beta should be float32 to preserve precision.'
+    if g is not None:
+        assert g.dtype == torch.float32, 'g should be float32 to preserve precision.'
     assert q.shape[-1] in [16, 32, 64, 128], "only support head_dim in [16, 32, 64, 128] for now. Stay tuned!"
     assert v.shape[-1] in [16, 32, 64, 128], "only support head_dim in [16, 32, 64, 128] for now. Stay tuned!"
     assert q.shape[-1] == k.shape[-1], 'q, k should have the same head_dim.'
@@ -265,6 +275,5 @@ def parallel_path_attn(
     assert q.shape[-2] % k.shape[-2] == 0, 'the number of query heads should be divisible by the number of key heads'
     o, k_cache = ParallelPATHAttentionFunction.apply(q, k, v, w, beta, g, scale, cu_seqlens, use_cache)
     return o, k_cache
-
 
 parallel_path_attention = parallel_path_attn

tests/ops/test_path_attn.py
@@ -16,7 +16,7 @@ def naive_path_attn(q, k, v, w, beta, g, scale, BT=64):
     original_dtype = q.dtype
     HQ = q.shape[2]
     H = k.shape[2]
-    q, k, v, w, beta, g = map(lambda x: x.to(torch.float32).transpose(1, 2), [q, k, v, w, beta, g])
+    q, k, v, w, beta, g = map(lambda x: x.to(torch.float).transpose(1, 2), [q, k, v, w, beta, g])
     g_cumsum = g.cumsum(-1)
     q = q.unsqueeze(2).expand(-1, -1, HQ//HQ, -1, -1).flatten(1, 2)
     k = k.unsqueeze(2).expand(-1, -1, HQ//H, -1, -1).flatten(1, 2)
@@ -95,8 +95,8 @@ def test_parallel(
     q = torch.randn((B, T, HQ, D), dtype=dtype, device=device).requires_grad_(True)
     k = torch.randn((B, T, H, D), dtype=dtype, device=device).requires_grad_(True)
     v = torch.randn((B, T, H, D), dtype=dtype, device=device).requires_grad_(True)
-    w = F.normalize(torch.randn((B, T, H, D), dtype=dtype, device=device), dim=-1, p=2).requires_grad_(True)
-    beta = torch.empty((B, T, H), dtype=dtype, device=device).uniform_(1.5, 2.0).requires_grad_(True)
+    w = F.normalize(torch.randn((B, T, H, D), dtype=torch.float, device=device), dim=-1, p=2).requires_grad_(True)
+    beta = torch.empty((B, T, H), dtype=torch.float, device=device).uniform_(1.5, 2.0).requires_grad_(True)
     if use_forget_gate:
         g = torch.empty((B, T, HQ), dtype=torch.float, device=device).uniform_(
             0.95, 1).log().requires_grad_(True)
@@ -171,8 +171,8 @@ def test_parallel_varlen(
     q = torch.randn((1, T, HQ, D), dtype=dtype, device=device).requires_grad_(True)
     k = torch.randn((1, T, H, D), dtype=dtype, device=device).requires_grad_(True)
     v = torch.randn((1, T, H, D), dtype=dtype, device=device).requires_grad_(True)
-    w = F.normalize(torch.randn((1, T, H, D), dtype=dtype, device=device), dim=-1, p=2).requires_grad_(True)
-    beta = torch.rand((1, T, H), dtype=dtype, device=device).sigmoid().requires_grad_(True)
+    w = F.normalize(torch.randn((1, T, H, D), dtype=torch.float, device=device), dim=-1, p=2).requires_grad_(True)
+    beta = torch.rand((1, T, H), dtype=torch.float, device=device).sigmoid().requires_grad_(True)
     if use_forget_gate:
         g = torch.empty((1, T, HQ), dtype=torch.float, device=device).uniform_(0.95, 1).log().requires_grad_(True)
     else: